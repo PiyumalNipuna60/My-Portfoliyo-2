af7651b841bdb54565b1d584b00a93cab2468a85
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-24 01:26:14 +0530

    Add Css style-switcher Theme light and dark mode jas

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -16,12 +16,11 @@ window.addEventListener("scroll", () => {
 const alternateStyle = document.querySelectorAll(".alternate-style");
 
 function setActiveStyle(color) {
-    alternateStyle.forEach((style)=>
-    {
+    alternateStyle.forEach((style) => {
         if (color === style.getAttribute("title")) {
             style.removeAttribute("disabled");
         } else {
-            style.setAttribute("disabled",true);
+            style.setAttribute("disabled", true);
 
         }
     })
@@ -30,13 +29,18 @@ function setActiveStyle(color) {
 
 /*====Theme light and dark mode====*/
 
-const  dayNight=document.querySelector(".day-night");
-window.addEventListener("load",()=>{
-    if (document.body.classList.contains("dark")){
-        dayNight.querySelector("i").classList.add("fa-sun");
-    }else {
-        dayNight.querySelector("i").classList.add("fa-moon");
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark")
+})
 
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    } else {
+        dayNight.querySelector("i").classList.add("fa-moon");
     }
 })
 
