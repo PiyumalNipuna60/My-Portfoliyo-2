17e55650ba52fedfa5610f06e477f40714433381
PiyumalNipuna60 <nipuna315np@gmail.com>
2024-09-09 23:55:51 +0530

    home page update

--- js/script.js
+++ js/script.js
@@ -13,3 +13,36 @@ $(document).ready(function () {
     style.innerHTML = `body::-webkit-scrollbar {display: none;}`;
     document.head.appendChild(style);
 });*/
+
+const stack = document.querySelector(".stack");
+const cards = Array.from(stack.children)
+  .reverse()
+  .filter((child) => child.classList.contains("card1"));
+
+cards.forEach((card1) => stack.appendChild(card1));
+
+function moveCard() {
+  const lastCard = stack.lastElementChild;
+  if (lastCard.classList.contains("card1")) {
+    lastCard.classList.add("swap");
+
+    setTimeout(() => {
+      lastCard.classList.remove("swap");
+      stack.insertBefore(lastCard, stack.firstElementChild);
+    }, 1200);
+  }
+}
+
+let autoplayInterval = setInterval(moveCard, 4000);
+
+stack.addEventListener("click", function (e) {
+  const card = e.target.closest(".card1");
+  if (card && card === stack.lastElementChild) {
+    card.classList.add("swap");
+
+    setTimeout(() => {
+      card.classList.remove("swap");
+      stack.insertBefore(card, stack.firstElementChild);
+    }, 1200);
+  }
+});
